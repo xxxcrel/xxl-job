0ab92b5d677f8e6039682fc51f1e8a5979cd56e7
wuxuecheng <wuxuecheng_wxc@mail.qzkeji.cn>
2022-07-07 16:36:40 +0800

    Add: 添加状态条

--- xxl-job-admin/src/main/resources/static/js/joblog.index.1.js
+++ xxl-job-admin/src/main/resources/static/js/joblog.index.1.js
@@ -69,6 +69,45 @@ $(function() {
         endDate: rangesConf[I18n.daterangepicker_ranges_today][1]
 	});
 
+    var percentBar = function(pShape, cText, cBorder, cBar, cBack, vRound, bType) {
+          pShape = pShape || 'square';
+          cText = cText || '#000';
+          cBorder = cBorder || '#BCBCBC';
+          cBar = cBar || '#5FD868';
+          cBack = cBack || '#E6E6E6';
+          vRound = vRound || 0;
+          bType = bType || 'ridge';
+          //Bar templates
+          var styleRule1 = 'max-width:100px;height:12px;margin:0 auto;';
+          var styleRule2 = 'border:2px '+bType+' '+cBorder+';line-height:12px;font-size:14px;color:'+cText+';background:'+cBack+';position:relative;';
+          var styleRule3 = 'height:12px;line-height:12px;text-align:center;background-color:'+cBar+';padding:auto 6px;';
+          //Square is default, make template round if pShape == round
+          if(pShape=='round'){
+            styleRule2 += 'border-radius:5px;';
+            styleRule3 += 'border-top-left-radius:4px;border-bottom-left-radius:4px;';
+          }
+
+          return function(data, type, row) {
+            if (data == null) {
+                return I18n.system_empty;
+            }
+            console.log("status: " + data);
+            s = parseFloat(data.toString().replace(/\s%|%/g,'')).toFixed(vRound);
+            //Not allowed to go over 100%
+            if(s>100){s=100}
+
+            // Order, search and type gets numbers
+            if (type !== 'display') {
+              return s;
+            }
+            if (typeof data !== 'number' && typeof data !== 'string') {
+              return data;
+            }
+
+            //Return the code for the bar
+            return '<div style="'+styleRule1+'"><div style="'+styleRule2+'"><div style="'+styleRule3+'width:'+s+ '%;"></div><div style="width:100%;text-align:center;position:absolute;left:0;top:0;">'+s+'%</div></div></div>';
+          };
+        };
 	// init date tables
 	var logTable = $("#joblog_list").dataTable({
 		"deferRender": true,
@@ -176,46 +215,7 @@ $(function() {
 	                {
                         "data": 'handleStatus',
                         "width":'10%',
-                        "render": function ( data, type, row ) {
-	                		return data?data:I18n.system_empty;
-//                            var percentBar = function(pShape, cText, cBorder, cBar, cBack, vRound, bType) {
-//                              pShape = pShape || 'square';
-//                              cText = cText || '#000';
-//                              cBorder = cBorder || '#BCBCBC';
-//                              cBar = cBar || '#5FD868';
-//                              cBack = cBack || '#E6E6E6';
-//                              vRound = vRound || 0;
-//                              bType = bType || 'ridge';
-//                              //Bar templates
-//                              var styleRule1 = 'max-width:100px;height:12px;margin:0 auto;';
-//                              var styleRule2 = 'border:2px '+bType+' '+cBorder+';line-height:12px;font-size:14px;color:'+cText+';background:'+cBack+';position:relative;';
-//                              var styleRule3 = 'height:12px;line-height:12px;text-align:center;background-color:'+cBar+';padding:auto 6px;';
-//                              //Square is default, make template round if pShape == round
-//                              if(pShape=='round'){
-//                                styleRule2 += 'border-radius:5px;';
-//                                styleRule3 += 'border-top-left-radius:4px;border-bottom-left-radius:4px;';
-//                              }
-//
-//                              return function(data, type, row) {
-//                                console.log("status: " + data);
-//                                s = parseFloat(d.toString().replace(/\s%|%/g,'')).toFixed(vRound);
-//                                //Not allowed to go over 100%
-//                                if(s>100){s=100}
-//
-//                                // Order, search and type gets numbers
-//                                if (type !== 'display') {
-//                                  return s;
-//                                }
-//                                if (typeof d !== 'number' && typeof d !== 'string') {
-//                                  return d;
-//                                }
-//
-//                                //Return the code for the bar
-//                                return '<div style="'+styleRule1+'"><div style="'+styleRule2+'"><div style="'+styleRule3+'width:'+s+ '%;"></div><div style="width:100%;text-align:center;position:absolute;left:0;top:0;">'+s+'%</div></div></div>';
-//                              };
-//                            };
-//                            return percentBar();
-                        }
+                        "render": percentBar()
                     },
 	                {
 						"data": 'handleMsg' ,
